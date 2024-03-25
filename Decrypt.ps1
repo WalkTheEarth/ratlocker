7ccdc0453a9dec3a7bef855188edde6461d926b0
WalkTheEarth <152687463+WalkTheEarth@users.noreply.github.com>
2024-03-25 18:01:01 +0000

    le decryption

---
+++ Decrypt.ps1
@@ -0,0 +1,8 @@
+﻿$dcrypt = Read-Host -Prompt "Enter your decryption password..."
+
+if ($dcrypt -eq '2us0jru04ws@gz@') {
+   Get-ChildItem -Path $HOME\Desktop, $HOME\Pictures, $HOME\Music, $HOME\Videos, $HOME\Downloads -Filter "*.ratl0ck3r" | Foreach-Object { Rename-Item $_ $_.Name.Replace(".ratl0ck3r", "") }
+   Write-Host "Files decrypted successfully!"
+} else {
+   Write-Host "Incorrect, its case sensitive."
+}
